f2d1f6fadad246f8d4bbb72002435ac2c4f2ead0
Nash McDonald <nash42192@gmail.com>
2019-02-16 17:00:29 -0800

    full game completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# unit-4-game
+This is a simple fighting game where you can choose a character, and click their image to deal damage to opponents.
+
+Choose your opponents wisely, as you can only win once you defeat all 3. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,111 +1,95 @@
 //Object Definitions
 var ShovelKnight = {
+        Name: 'Shovel Knight',
          Id : '#Shovel',
          SRC: "assets/images/Shovel_Knight.png",
      Base_HP: 100, // Stores base Hit Points
           HP: 100,  // Stores Hit Points or “health”
 
-    BaseAtk : 6,	// Stores base Attack value
-        Atk : 6, 	//Stores Attack power incremented by AtkInc, initialized as BaseAtk
-     AtkInc : 6,	//Stores Attack value increase after each hit when played by user
+    BaseAtk : 9,	// Stores base Attack value
+        Atk : 9, 	//Stores Attack power incremented by AtkInc, initialized as BaseAtk
+     AtkInc : 9,	//Stores Attack value increase after each hit when played by user
 
-     CtrAtk : 12, 	//Stores Counter Attack value
-
-    AtkName : function(EnemyHp, Stats) {
-        this.HP = 1;
-      //Have individual attacks per character that changes enemy hp based on certain stats, like crit chances, etc.. (get base attacks down first)
-    },
+     CtrAtk : 50, 	//Stores Counter Attack value
 
     ResStat : function() {
-      this.HP = 6;
-      this.BaseAtk = 6;
-      this.Atk = 6;
-      this.CtrAtk = 6;
+      this.HP = 100;
+      this.BaseAtk = 9;
+      this.Atk = 9;
+      this.CtrAtk = 50;
     }
 
 };
 
 var BlackMage = { 
+       Name: 'Black Mage',
          Id: '#Mage',
         SRC: "assets/images/Black_Mage.png",
     Base_HP: 200, // Stores base Hit Points
          HP: 200,  // Stores Hit Points or “health”
 
-    BaseAtk : 6,	// Stores base Attack value
-      Atk : 6, 	//Stores Attack power incremented by AtkInc, initialized as BaseAtk
-    AtkInc : 6,	//Stores Attack value increase after each hit when played by user
+    BaseAtk : 8,	// Stores base Attack value
+      Atk : 8, 	//Stores Attack power incremented by AtkInc, initialized as BaseAtk
+    AtkInc : 8,	//Stores Attack value increase after each hit when played by user
 
-    CtrAtk : 12, 	//Stores Counter Attack value
-
-    AtkName : function(EnemyHp, Stats) {
-      this.HP = 1;
-    //Have individual attacks per character that changes enemy hp based on certain stats, like crit chances, etc.. (get base attacks down first)
-    },
+    CtrAtk : 15, 	//Stores Counter Attack value
 
     ResStat : function() {
-    this.HP = 6;
-    this.BaseAtk = 6;
-    this.Atk = 6;
-    this.CtrAtk = 6;
+    this.HP = 200;
+    this.BaseAtk = 8;
+    this.Atk = 8;
+    this.CtrAtk = 15;
     }
 
 };
 
-var HollowKnight = { 
+var HollowKnight = {
+      Name: 'Hollow Knight', 
         Id: '#Hollow',
        SRC: "assets/images/Hollow_Knight.png",
-   Base_HP: 300, // Stores base Hit Points
-        HP: 300,  // Stores Hit Points, always Base_HP intially
+   Base_HP: 250, // Stores base Hit Points
+        HP: 250,  // Stores Hit Points, always Base_HP intially
 
     BaseAtk : 6,	// Stores base Attack value
       Atk : 6, 	//Stores Attack power incremented by AtkInc, initialized as BaseAtk
     AtkInc : 6,	//Stores Attack value increase after each hit when played by user
 
-    CtrAtk : 12, 	//Stores Counter Attack value
-
-    AtkName : function(EnemyHp, Stats) {
-      this.HP = 1;
-    //Have individual attacks per character that changes enemy hp based on certain stats, like crit chances, etc.. (get base attacks down first)
-    },
+    CtrAtk : 8, 	//Stores Counter Attack value
 
     ResStat : function() {
-    this.HP = 6;
+    this.HP = 250;
     this.BaseAtk = 6;
     this.Atk = 6;
-    this.CtrAtk = 6;
+    this.CtrAtk = 8;
     }
 
 };
 
 var Megaman = {
+      Name: 'Megaman',
         Id: '#Mega',
        SRC: "assets/images/Megaman.png",
-   Base_HP: 400, // Stores base Hit Points
-        HP: 400,  // Stores Hit Points or “health”
+   Base_HP: 300, // Stores base Hit Points
+        HP: 300,  // Stores Hit Points or “health”
 
     BaseAtk : 6,	// Stores base Attack value
       Atk : 6, 	//Stores Attack power incremented by AtkInc, initialized as BaseAtk
     AtkInc : 6,	//Stores Attack value increase after each hit when played by user
 
-    CtrAtk : 12, 	//Stores Counter Attack value
-
-    AtkName : function(EnemyHp, Stats) {
-      this.HP = 1;
-    //Have individual attacks per character that changes enemy hp based on certain stats, like crit chances, etc.. (get base attacks down first)
-    },
+    CtrAtk : 7, 	//Stores Counter Attack value
 
     ResStat : function() {
-    this.HP = 6;
+    this.HP = 300;
     this.BaseAtk = 6;
     this.Atk = 6;
-    this.CtrAtk = 6;
+    this.CtrAtk = 7;
     }
 
 };
 
 var Blank_Char = {
  SRC: "assets/images/blank.jpg",
-Base_HP: 0, // Stores base Hit Points
+Base_HP: 10, // Stores base Hit Points
   HP: 0,  // Stores Hit Points or “health”
 
 BaseAtk : 0,	// Stores base Attack value
@@ -148,6 +132,7 @@ function PC_Choice(){
   PC_Chosen = true;
   $(ChseBtn).html("");
   $(PC.Id).css('background-color','green');
+  $("#Instructions").html("Click a Sprite to Select Opponent!");
 }
 
 function NPC_Choice(){
@@ -155,6 +140,7 @@ function NPC_Choice(){
   FightStart = true;
   $(NPC.Id).css('background-color','yellow');
   Stat_Update(PC,NPC);
+  $("#Instructions").html("Click your character in the ring fight!");
   
 }
 
@@ -164,8 +150,8 @@ function Stat_Update(PC_HP,NPC_HP){
   var PC_Str;
   var NPC_Str;
 
-  $("#PC_HP").html('<div></div>').addClass('HP-Bar');
-  $("#NPC_HP").html('<div></div>').addClass('HP-Bar');
+  $("#PC_HP").addClass('HP-Bar');
+  $("#NPC_HP").addClass('HP-Bar');
 
   PC_Width = ((PC_HP.HP)/(PC_HP.Base_HP)*100);
   NPC_Width = ((NPC_HP.HP)/(NPC_HP.Base_HP)*100);
@@ -186,26 +172,83 @@ function Stat_Update(PC_HP,NPC_HP){
   
 }
 
+function NewNPC(){
+  FightStart = false;
+  NPC.HP = 0;
+  Stat_Update(PC,NPC);
+  $(NPC.Id).css('background-color','red');
+  Defeated.push(NPC.Id);
+  NPC = Blank_Char;
+  updateNPC(NPC);
+  $(ChseBtn).html("");
+  $("#Instructions").html("Choose Another Opponent!");
+
+  console.log("opponents defeated = "+Defeated.length);
+  if(Defeated.length == 3)
+  {
+    $("#Instructions").html("You Win!");
+    ResetGame();
+  }
+  
+}
+
+function ResetGame(){
+
+  PC = Blank_Char;
+  NPC = Blank_Char;
+
+  $("#PC").attr("src", PC.SRC);
+  $("#NPC").attr("src", NPC.SRC);
+
+  $("#CharStats_PC").html('test');
+
+  $("#CharStats_NPC").html('test');
+
+  ShovelKnight.ResStat();
+  BlackMage.ResStat();
+  HollowKnight.ResStat();
+  Megaman.ResStat();
+
+  $(ShovelKnight.Id).css('background-color','white');
+  $(BlackMage.Id).css('background-color','white');
+  $(HollowKnight.Id).css('background-color','white');
+  $(Megaman.Id).css('background-color','white');
+
+  $(ChseBtn).html("");
+
+  Stat_Update(PC,NPC);
+  FightStart = false;
+  PC_Chosen = false;
+  Defeated = [];
+
+}
+
 function Attack(){
+
   NPC.HP = NPC.HP-PC.Atk;
   PC.Atk = PC.Atk+PC.BaseAtk;
+
   if(NPC.HP <= 0){
-    FightStart = false;
-    NPC.HP = 0;
-    $(NPC.Id).css('background-color','red');
-    Defeated.push(NPC.Id);
-    NPC = Blank_Char;
-    updateNPC(NPC);
-    $(ChseBtn).html("");
+    
+    NewNPC();
 
   }
   else{
   PC.HP = PC.HP-NPC.CtrAtk;
   }
+
+  if( (PC.HP <= 0) && (NPC.HP!=0)){
+    PC.HP = 0;
+    $("#Instructions").html("You Lose!");
+    ResetGame();
+  }
 }
 
 // Start Code Execution
 $(document).ready(function() {
+
+    $("#Instructions").html("Click a Sprite to Select Fighter!");
+    Stat_Update(PC,NPC);
 
     $( "#Shovel_Knight" ).click(function() {
       if( (!FightStart) && (!Defeated.includes('#Shovel') ) ){
